2f86b3bee233e1df968c6fd9d4ce334d9261c644
hacpaka <hacpaka@eggbe.com>
2015-06-20 23:38:35 +0300

    +

---
+++ src/commands/HashFindCommand.php
@@ -0,0 +1,43 @@
+<?php
+use \Illuminate\Console\Command;
+use \Symfony\Component\Console\Input\InputOption;
+use \Symfony\Component\Console\Input\InputArgument;
+
+class HashFindCommand extends Command {
+
+	/**
+	 * The console command name.
+	 * @var string
+	 */
+	protected $name = 'hash:find';
+
+	/**
+	 * Execute the console command.
+	 */
+	public function fire() {
+		if (!($hash = App::make('HashStore')->find($this->argument('key')))) {
+			$this->line('Hash not exits!');
+		}else {
+			$this->line('Hash finded: ' . $hash);
+		}
+	}
+
+	/**
+	 * Get the console command arguments.
+	 * @return array
+	 */
+	protected function getArguments() {
+		return [
+			array('key', InputArgument::REQUIRED, ''),
+		];
+	}
+
+	/**
+	 * Get the console command options.
+	 * @return array
+	 */
+	protected function getOptions() {
+		return [];
+	}
+
+}
